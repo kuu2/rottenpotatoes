fe0dec8f2535d9e6619efa4afc1706c338acdc5b
kuu2 <pikachu025_@hotmail.com>
2015-02-18 00:15:38 -0400

    adding files to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = 'd6e5bb51e3a630e8335714e3f637b8c33c09c34c6159cf48ddd6689754013ed1e0dde2f635d3638ab114a25d05e9afd2359bc9f7c483c72cc58fc62917e6a8fc'
